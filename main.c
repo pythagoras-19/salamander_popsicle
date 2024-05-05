2a5ddb29b94dc0e8b348b83f78a7bdde5a1053aa
Matt Christiansen <mattchristiansen59@gmail.com>
2024-05-04 23:10:52 -0700

    setups

--- main.c
+++ main.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
-void toCelsius();
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+void to_celsius();
+void read_lines();
 
 int main() {
     printf("Hello, World!\n");
-    toCelsius();
+    to_celsius();
+    read_lines();
     return 0;
 }
 
-
-void toCelsius() {
+void to_celsius() {
     int fahrenheit, celsius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
+    fahrenheit = LOWER;
 
-    fahrenheit = lower;
-    while (fahrenheit <= upper) {
+    while (fahrenheit <= UPPER) {
         celsius = 5 * (fahrenheit - 32) / 9;
         printf("%d\t%d\n", fahrenheit, celsius);
-        fahrenheit = fahrenheit + step;
+        fahrenheit = fahrenheit + STEP;
     }
+}
+
+void read_lines() {
+    FILE *fptr;
+    const char* filePath = (const char *) fopen("./md.txt", "r");
+    if (filePath == NULL) {
+        printf("FILE_PATH environment variable is not set.\n");
+        exit(EXIT_FAILURE);
+    }
+    fptr = fopen(filePath, "r");
+    if (fptr == NULL) {
+        perror("Failed to open file");
+        printf("Error opening file: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    int c = fgetc(fptr);
+    while (c != EOF) {
+        putchar(c);
+        c = fgetc(fptr);
+    }
+
+    fclose(fptr);
 }
